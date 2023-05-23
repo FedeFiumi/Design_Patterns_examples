f68b7385d65e2019a26da7ed147a222462e8d06c
FedeFiumi <fedefiumi@hotmail.it>
2023-05-24 00:35:23 +0200

    Add cpp version of 2 behavioral patterns: State, Chain of responsibility

---
+++ behavioral/chain_of_responsability/cpp/generic_example.cpp
@@ -0,0 +1,63 @@
+/* The Chain of Responsibility Pattern is a behavioral design pattern that allows
+   an object to pass a request along a chain of potential handlers until the request
+   is handled or reaches the end of the chain. Each handler in the chain has the ability
+   to either handle the request or pass it to the next handler in the chain.
+   This pattern decouples the sender of the request from its receivers, giving multiple
+   objects the opportunity to handle the request independently.
+*/
+
+#include <iostream>
+
+class Handler {
+public:
+    virtual void setNext(Handler* handler) = 0;
+    virtual void handleRequest(int requestCode) = 0;
+};
+
+class ConcreteHandler: public Handler {
+public:
+    void setNext(Handler* handler) override {
+        delete nextHandler_;
+        nextHandler_ = handler;
+    }
+
+    void handleRequest(int requestCode) override {
+        if ((requestCode > 0) && (requestCode < 10)) {
+            std::cout << "Request " << requestCode << " handled." << std::endl;
+        } else if (nextHandler_ != nullptr) {
+            std::cout << "Passed to handle at: " << this << std::endl;
+            nextHandler_->handleRequest(requestCode);
+        } else {
+            std::cout << "Request " << requestCode << " can't be handled." << std::endl;
+        }
+    }
+
+private:
+    Handler* nextHandler_;
+};
+
+// Client code
+int main() {
+    Handler* handler1 = new ConcreteHandler();
+    Handler* handler2 = new ConcreteHandler();
+    Handler* handler3 = new ConcreteHandler();
+
+    handler1->setNext(handler2);
+    handler2->setNext(handler3);
+
+    // Handled
+    handler1->handleRequest(3);
+    // Not Handled
+    handler1->handleRequest(0);
+    // Not Handled
+    handler1->handleRequest(100);
+
+    // Clean up
+    delete handler1;
+    delete handler2;
+    delete handler3;
+
+    std::cout << "PRESS ENTER:";
+    std::cin.get();
+    return 0;
+}
---
+++ behavioral/state/cpp/generic_example.cpp
@@ -0,0 +1,84 @@
+/* The State Pattern is a behavioral design pattern that allows an object to alter
+   its behavior when its internal state changes. It encapsulates different behaviors
+   into separate state objects and delegates the execution of behavior to the current
+   state object.
+   This pattern promotes loose coupling between the object and its behavior, making it
+   easier to add or modify states without impacting the object's code.
+*/
+
+#include <iostream>
+
+// Context class that contains a state
+class Context;
+
+// Abstract State class
+class State {
+public:
+    virtual void handle(Context& context) = 0;
+};
+
+// Concrete State classes
+class StateA : public State {
+public:
+    void handle(Context& context) override;
+};
+
+class StateB : public State {
+public:
+    void handle(Context& context) override;
+};
+
+class StateC : public State {
+public:
+    void handle(Context& context) override;
+};
+
+// Context class
+class Context {
+private:
+    State* currentState_;
+
+public:
+    Context() : currentState_(new StateA()) {}
+
+    void setState(State* state) {
+        delete currentState_;
+        currentState_ = state;
+    }
+
+    void request() {
+        currentState_->handle(*this);
+    }
+};
+
+// Concrete State A implementation
+void StateA::handle(Context& context) {
+    std::cout << "State A handled the request." << std::endl;
+    context.setState(new StateB());
+}
+
+// Concrete State B implementation
+void StateB::handle(Context& context) {
+    std::cout << "State B handled the request." << std::endl;
+    context.setState(new StateC());
+}
+
+// Concrete State C implementation
+void StateC::handle(Context& context) {
+    std::cout << "State C handled the request." << std::endl;
+    context.setState(new StateA());
+}
+
+// Client code
+int main() {
+    Context context;
+
+    context.request();  // Output: State A handled the request.
+    context.request();  // Output: State B handled the request.
+    context.request();  // Output: State C handled the request.
+    context.request();  // Output: State A handled the request.
+
+    std::cout << "PRESS ENTER:";
+    std::cin.get();
+    return 0;
+}
