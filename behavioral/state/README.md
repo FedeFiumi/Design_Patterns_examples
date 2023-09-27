5c31b488467c3192bb64c172f8eb57ceec9b9123
Federico <fedefiumi@hotmail.it>
2023-09-27 23:21:06 +0200

    Remove description to store it in the readme

---
+++ behavioral/state/README.md
@@ -0,0 +1,7 @@
+# State pattern
+
+This pattern that allows an object to alter its behavior when its internal state changes.
+It encapsulates different behaviors into separate state objects and delegates the
+execution of behavior to the current state object.
+This pattern promotes loose coupling between the object and its behavior, making it easier
+to add or modify states without impacting the object's code.
--- behavioral/state/cpp/generic_example.cpp
+++ behavioral/state/cpp/generic_example.cpp
@@ -1,11 +1,3 @@
-/* The State Pattern is a behavioral design pattern that allows an object to alter
-   its behavior when its internal state changes. It encapsulates different behaviors
-   into separate state objects and delegates the execution of behavior to the current
-   state object.
-   This pattern promotes loose coupling between the object and its behavior, making it
-   easier to add or modify states without impacting the object's code.
-*/
-
 #include <iostream>
 
 // Context class that contains a state
