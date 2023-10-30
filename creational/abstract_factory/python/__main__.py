0d550c31c76272252f95ae3d50524efd440614d1
Federico <fedefiumi@hotmail.it>
2023-10-30 22:37:20 +0100

    Add main function

---
+++ creational/abstract_factory/python/__main__.py
@@ -0,0 +1,17 @@
+from factories.ford_factory import FordFactory
+from factories.mercedes_factory import MercedesFactory
+from factories.toyota_factory import ToyotaFactory
+
+
+for factory in [FordFactory, MercedesFactory, ToyotaFactory]:
+    print("Factory " + str(factory))
+    car = factory.create_low_range()
+    car.start()
+    car.stop()
+    car = factory.create_mid_range()
+    car.start()
+    car.stop()
+    car = factory.create_high_range()
+    car.start()
+    car.stop()
+    print()
